6543a3b362eca360211f54d157308e685c1a2e21
Daniel <danieliozzi@gmail.com>
2016-04-21 09:55:48 +0200

    css+ (dev) +DataSource interface

--- src/main.ts
+++ src/main.ts
@@ -2,41 +2,73 @@
 ///<reference path="references.d.ts"/>
 ///<reference path="../typings/tsd.d.ts"/>
 
+interface DataSource {
+    //required
+    key:string ;
+
+    urls : {
+        //required
+        getter: string
+    }
+
+    columnMaps?:ColumnMap[];
+    /***
+     * Has to be populated here,
+     */
+    items?:wx.IObservableList<{}>;
+}
+
 class MainViewModel {
 
     brand = wx.property('tiny-x');
 
-    items = wx.list();
+    //items = wx.list();
 
-    columnMaps: ColumnMap[] = [{
-        key: 'created',
-        displayName: 'Created',
-        inputType: 'date',
-        converter: 'string-to-date-converter'
-    },{
-        key: 'modified',
-        displayName: 'Modified',
-        inputType: 'date',
-        converter: 'string-to-date-converter'
-    }];
+    dataSource = wx.property<DataSource>();
+
+    dataSources = wx.list<DataSource>();
 
     constructor() {
 
-        fetch('../data/materials.json')
-            .then(r => r.json())
-            .then(items => {
-                this.items.addRange(items);
-            })
-            .catch(e=> {
-                console.log(`Error: ${e}`);
-            });
+        fetch('../data/datasource.json').then(r=>r.json()).then( (sources: DataSource[]) =>{
+            for(var source of sources){
+                // initialize the prop, is not there
+                // datasource will provide url's for get/update/insert || get/post ? ?
+                source.items = wx.list();
+            }
+            this.dataSources.addRange(sources);
+        });
+
+        var load = ()=> {
+            fetch(this.dataSource().urls.getter)
+                .then(r => r.json())
+                .then(items => {
+
+                    if(this.dataSource().items){
+                        this.dataSource().items.clear();
+                        this.dataSource().items.addRange(items);
+                        return;
+                    }
+                    this.dataSource().items = wx.list(items);
+                })
+                .catch(e=> {
+                    console.log(`Error: ${e}`);
+                });
+        };
+
+        //load();
+
+        this.dataSource.changed.subscribe(()=> {
+            load();
+        });
+
     }
 }
 
 wx.app.devModeEnable();
 
-wx.app.filter('string-to-date-converter', function(input: string) {
-    return input ;//? moment(input).toDate() : null;
+wx.app.filter('string-to-date-converter', function (input:string) {
+    return input;//? moment(input).toDate() : null;
 });
 
 
